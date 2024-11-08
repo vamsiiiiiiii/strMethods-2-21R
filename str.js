792b9ad5d0b30b641d3391ead83c55c5ffa0c828
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-11-08 16:03:18 +0530

    str methods part-2

---
+++ str.js
@@ -0,0 +1,67 @@
+// let a="vamsi@gmail.com"
+
+// const b=a.startsWith("vamsi")
+// console.log(b);
+
+// let b="vamsiEnduri";
+// console.log(b.startsWith("Enduri"));
+
+
+// let a="resume.pdf"
+// let a="intro.mp4"
+// let b=".mkv"
+// console.log(a.endsWith(b));
+
+
+// let a="developer role is to developer the website and also developer role is to maintain good ui with functionalities"
+
+// // console.log(a.lastIndexOf("developer"));
+// console.log(a.lastIndexOf("is"));
+
+
+
+// const a="save60% \n"
+
+// console.log(a.repeat(10));
+
+// const a="in the stcok";
+// console.log(a.replace("in the","out of"));
+
+// const a=["vamsi","ravi"];
+// const b=a.splice(1,1,"ram","sam")
+// console.log(a);
+// console.log(b);
+
+
+// const a=["vamsi","ravi"];
+// const b=a.splice(1,0,"ram","sam")
+// console.log(a);
+// console.log(b);
+
+// const a="hello"
+// const b=a.split("")
+// const c=b.splice(1,3,true,"ok","hi")
+// console.log(c);
+// console.log(b);
+
+// const a="vamsiEV";
+// const b=a.substring(9)
+// console.log(b);
+
+// const a=123;
+// console.log( typeof a);
+
+// console.log(typeof a.toString());
+// const b=[1,2,3];
+// console.log(typeof b.toString());
+
+// const a=["vamsi","ravi","raju","ram","sam"];
+// console.log(a.slice(1,4));
+// console.log(a);
+
+
+// console.log(a.splice(0,3,true,false));
+// console.log(a);
+
+
+
